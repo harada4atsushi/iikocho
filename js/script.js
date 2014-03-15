2b44f9f96a6cba31785ffbd846cd91c085af6c82
maasa <maasa.giss2@gmail.com>
2014-03-15 20:06:55 +0900

    月を表示・スタンプ画像変更

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 // DEMO
 localStorage[201401] = true;
 localStorage[201402] = true;
+localStorage[201403] = true;
+localStorage[201404] = true;
+localStorage[201405] = true;
+localStorage[201406] = true;
+localStorage[201407] = true;
+localStorage[201408] = true;
+localStorage[201409] = true;
+localStorage[201410] = true;
   
 $(function(){
 
@@ -8,27 +16,26 @@ $(function(){
 	var year  = date.getFullYear() ;
 	var month = ('0'+(date.getMonth()+1)).slice(-2);
 
-	var img = '<img src="./img/santa.png" />';
-
 	var stamp_load = function(){
 		if(! localStorage) return;
 		var count = 0;
 		for(var i = 1; i <= 12; i++) {
 			var m = ('0'+i).slice(-2);
 			if(localStorage[year+m]){
-				$('#stamp_list #month_'+m).append(img);
+				$('#stamp_list #month_'+m).find('img').attr('src', './img/hatClear.png');
 				count++;
 			}
 		}
 		if(count == 12){
 			$('#congratulation').show();
 			(new Audio('./sound/congratulation.mp3')).play();
+			localStorage.clear();
 		}
 	}
 
 	stamp_load();
 
-	$('#stamp_list li').on('click', function(){
+	$('#stamp_list td').on('click', function(){
 		var month = $(this).attr('id').replace('month_', '');
 		localStorage[year+month] = true;
 		window.location = 'index.html?ym='+year+month;
